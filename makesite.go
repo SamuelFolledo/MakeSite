6175ecea91393f92b04b154bf63de70dc9a361e7
Samuel Folledo <samuelfolledo@gmail.com>
2020-01-28 20:09:53 -0800

    [wip] ERROR do not pull. Currently have a weird bug

--- makesite.go
+++ makesite.go
@@ -1,9 +1,17 @@
 package main
 
 import (
-	"Io/ioutil"
-	"fmt"
+	"fmt" //format
+	"html/template" //allows us to do templating
+	"io/ioutil"
+	"net/http"
+	"oset/http"
 )
+
+type FileLines struct {
+	Title   string //capital means public, small means private
+	Message string
+}
 
 func main() {
 	line, err := readFile("first-post.txt")
@@ -12,6 +20,36 @@ func main() {
 	} else {
 		fmt.Print(line)
 	}
+
+	// paths := []string{
+	// 	"html/layout.html", //1h24m gotta have a template
+	// }
+
+	// news := FileLines{
+	// 	Title: "EYOOOO", Message: line,
+	// }
+	
+	// t := template.ParseFiles("layout.html") //template loader //1h25m is how it is actually read
+	// err = t.Execute(os.Stdout, news)        //1h26m Stdout prints it in the terminal
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// tmpl := template.Must(template.ParseFiles("html/layout.html"))
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	tmpl.Execute(w, news)
+	// })
+	// http.ListenAndServe(":80", nil)
+
+	// t, err := template.ParseFiles("./html/first.html")
+	// if err == nil {
+	// 	log.Println("Template parsed successfully....")
+	// }
+	// err := templates.ExecuteTemplate(w, "html/first.html", nil)
+	// if err != nil {
+	// 	log.Println("Not Found template")
+	// }
+	// t.Execute(w, news)
 }
 
 func readFile(fileName string) (line string, errorMessage error) { //method that will read a file and return lines or error
